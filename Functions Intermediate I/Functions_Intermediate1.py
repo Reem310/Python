026b3394759e92a8cfaffd632afc8fecf7271aaf
Reem Bm <ryame.3@gmail.com>
2022-06-05 23:43:20 +0300

    git

---
+++ Functions Intermediate I/Functions_Intermediate1.py
@@ -0,0 +1,9 @@
+from random import random
+
+
+def randInt(min=0,max=100):
+    rand_num=round((random()*(max-min))+min)
+    return rand_num
+
+for i in range(100):
+    print(randInt(min=50,max=99))
