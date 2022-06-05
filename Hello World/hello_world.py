ed0b20514415e96ac7d1fd3f9846b9adce12952c
Reem Bm <ryame.3@gmail.com>
2022-06-05 19:28:09 +0300

    git

---
+++ Hello World/hello_world.py
@@ -0,0 +1,11 @@
+print("Hello World")
+a = "Reem"
+print ("Hello", a)
+print ("Hello " + a)
+fav_num=3
+print ("Hello", fav_num)
+print("Hello "+str(fav_num))
+food1="Pizza"
+food2="Burger" 
+print("I love to eat {} and {}".format(food1,food2))
+print(f"I love to eat {food1} and {food2}")
