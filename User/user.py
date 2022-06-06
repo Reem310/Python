9fc2e22df2b3b472d20b4669d94694f109900dfc
Reem Bm <ryame.3@gmail.com>
2022-06-06 16:07:36 +0300

    git

--- User/user.py
+++ User/user.py
@@ -6,22 +6,34 @@ class User:
 
     def make_deposit(self, amount):  # takes an argument that is the amount of the deposit
         self.account_balance += amount
-        return self
+        
 
     # takes an argument that is the amount of the withdrawl
     def make_withdrawal(self, amount):
         # the specific user's account decreases by the amount of the value received
         self.account_balance -= amount
-        return self
+        
 
     def display_user_balance(self):
         print(f"Name: {self.name}\nBalance: {self.account_balance}")
-        return self
+        
 
 
 guido = User("Guido van Rossum", "guido@python.com", 100)
 monty = User("Monty Python", "monty@python.com", 50)
 joe = User("Joe Bell", "joe@python.com",250)
-guido.make_deposit(10).make_deposit(10).make_deposit(30).make_withdrawal(10).display_user_balance()
-monty.make_deposit(10).make_deposit(10).make_withdrawal(30).make_withdrawal(10).display_user_balance()
-joe.make_deposit(50).make_withdrawal(10).make_withdrawal(20).make_withdrawal(10).display_user_balance()
+guido.make_deposit(10)
+guido.make_deposit(10)
+guido.make_deposit(30)
+guido.make_withdrawal(10)
+guido.display_user_balance()
+monty.make_deposit(10)
+monty.make_deposit(10)
+monty.make_withdrawal(30)
+monty.make_withdrawal(10)
+monty.display_user_balance()
+joe.make_deposit(50)
+joe.make_withdrawal(10)
+joe.make_withdrawal(20)
+joe.make_withdrawal(10)
+joe.display_user_balance()
