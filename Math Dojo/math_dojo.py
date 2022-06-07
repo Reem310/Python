8b54e108f242d93a449a2c9b007eb7cbe49211d8
Reem Bm <ryame.3@gmail.com>
2022-06-07 15:16:36 +0300

    git

--- Math Dojo/math_dojo.py
+++ Math Dojo/math_dojo.py
@@ -4,20 +4,21 @@ import xdrlib
 class MathDojo:
     def __init__(self):
         self.result = 0
+
     def add(self, num, *nums):
         # your code here
         self.result=self.result+num
-        if len(nums)>0:
+        if len(nums)>=0:
             for a in nums:
-                self.result=self.result+a
+                self.result+=a
         return self
 
     def subtract(self, num, *nums):
         # your code here
         self.result=self.result-num
-        if len(nums)>0:
+        if len(nums)>=0:
             for a in nums:
-                self.result=self.result-a
+                self.result-=a
         return self
         
 # create an instance:
@@ -26,12 +27,12 @@ bd = MathDojo()
 cd = MathDojo()
 
 # to test:
-x = ad.add(2).add(2,5,1).subtract(3,2).result
-print(x)	# should print 5
+a = ad.add(2).add(2,5,1).subtract(3,2).result
+print(a)	# should print 5
 # run each of the methods a few more times and check the result!
 b = bd.add(2).add(1,2).add(1,2,3).result
 print(b)
-c = cd.subtract(2).subtract(1,2).subtract(1,2,3).result
+c = cd.subtract(1,2).subtract(1,2,3).result
 print(c)
 
 
